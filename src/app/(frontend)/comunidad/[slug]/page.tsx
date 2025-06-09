4de89da835e9d34140536867c343e3f555b7f3ff
Felipe Trejos <felipetrejoslacayo2015@hotmail.com>
2025-06-09 21:00:34 +0200

    fix: Remove eslint checks entirely and fix props to the comunidad/[slug] page

--- src/app/(frontend)/comunidad/[slug]/page.tsx
+++ src/app/(frontend)/comunidad/[slug]/page.tsx
@@ -2,8 +2,8 @@ import { notFound } from 'next/navigation';
 import { fetchCategories } from '@/lib/api';
 import CategorySlug from '@/components/CategorySlug';
 
-export default async function CategoryPage({ params }: { params: { slug: string } }) {
-  const { slug } = params;
+export default async function CategoryPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
   
   const categories = await fetchCategories();
   const category = categories.find(cat => cat.slug === slug);
